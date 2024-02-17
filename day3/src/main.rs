fcc2499c6a1c8c6f02f9c1f7b003c05dff0df711
Enzo Pedro Bonacina <epbonacina@gmail.com>
2024-02-17 17:22:56 -0300

    A small refactoring section.

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -2,7 +2,6 @@ use std::fs;
 
 const INPUT_FILE_PATH: &str = "input.txt";
 
-
 fn read_input_file() -> Vec<String> {
     fs::read_to_string(INPUT_FILE_PATH)
         .expect("Couldn't read input file")
@@ -11,87 +10,64 @@ fn read_input_file() -> Vec<String> {
         .collect()
 }
 
-
-fn get_numbers_adjacent_to_symbols(lines: Vec<String>) -> Vec<i32>{
+fn get_numbers_adjacent_to_symbols(lines: &Vec<String>) -> Vec<u32> {
     let mut numbers_adjacent_to_symbols = Vec::new();
-    let mut number = String::new();
-    let mut found_relevant_number=false;
+
     for (y, line) in lines.iter().enumerate() {
+        let mut tmp_number = String::new();
+        let mut found_a_relevant_number = false;
+
         for (x, ch) in line.chars().enumerate() {
-            if ch.is_digit(10) {
-                if is_adjacent_to_symbol(x, y, &lines) {
-                    found_relevant_number = true;
+            match ch {
+                digit if ch.is_digit(10) && is_adjacent_to_symbol(&lines, x, y) => {
+                    tmp_number.push(digit);
+                    found_a_relevant_number = true;
                 }
-                number.push(ch);
-            }
-            else {
-                if found_relevant_number {
-                    numbers_adjacent_to_symbols.push(number.clone().parse().unwrap());
-                    found_relevant_number = false;
+                digit if ch.is_digit(10) => tmp_number.push(digit),
+                _ if found_a_relevant_number => {
+                    numbers_adjacent_to_symbols.push(tmp_number.parse().unwrap());
+                    found_a_relevant_number = false;
+                    tmp_number.clear();
                 }
-                number.clear();
+                _ => {
+                    tmp_number.clear();
+                }
             }
         }
-        if found_relevant_number {
-            numbers_adjacent_to_symbols.push(number.clone().parse().unwrap());
-            found_relevant_number = false;
+        if found_a_relevant_number {
+            numbers_adjacent_to_symbols.push(tmp_number.parse().unwrap());
         }
-        number.clear();
     }
     numbers_adjacent_to_symbols
 }
 
+fn is_adjacent_to_symbol(lines: &Vec<String>, x: usize, y: usize) -> bool {
+    let candidates_coords = [
+        (y.wrapping_sub(1), x.wrapping_sub(1)),
+        (y.wrapping_sub(1), x),
+        (y.wrapping_sub(1), x.wrapping_add(1)),
+        (y, x.wrapping_sub(1)),
+        (y, x.wrapping_add(1)),
+        (y.wrapping_add(1), x.wrapping_sub(1)),
+        (y.wrapping_add(1), x),
+        (y.wrapping_add(1), x.wrapping_add(1)),
+    ];
 
-fn is_adjacent_to_symbol(x: usize, y: usize, lines: &Vec<String>) -> bool {
-    let number_of_lines = lines.len();
-    let line_length = lines[0].len();
-
-    let y_start;
-    if y as i32 -1 >= 0 {
-        y_start = y-1;
-    }
-    else {
-        y_start = y;
-    }
-
-    let y_end;
-    if y+1 < number_of_lines {
-        y_end = y+1;
-    }
-    else {
-        y_end = y;
-    }
-    
-    let x_start;
-    if x as i32 -1 >= 0 {
-        x_start = x-1;
-    }
-    else {
-        x_start = x;
-    }
-
-    let x_end;
-    if x+1 < line_length {
-        x_end = x+1;
-    }
-    else {
-        x_end = x;
-    }
-
-    for line in lines[y_start..=y_end].iter() { 
-        for ch in line.chars().collect::<Vec<char>>()[x_start..=x_end].iter() { 
-            if !ch.is_digit(10) && ch != &'.' {
-                return true;
+    for (candidate_y, candidate_x) in candidates_coords {
+        if let Some(line) = lines.get(candidate_y) {
+            if let Some(value) = line.chars().nth(candidate_x) {
+                if !value.is_digit(10) && value != '.' {
+                    return true;
+                }
             }
         }
     }
     return false;
 }
 
-
 fn main() {
     let lines = read_input_file();
-    let numbers = get_numbers_adjacent_to_symbols(lines);
+    let numbers = get_numbers_adjacent_to_symbols(&lines);
     let mut sum = 0;
 
     for number in numbers {
