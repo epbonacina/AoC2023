f0dafada427293637febd92902799400140e0f6e
Enzo Pedro Bonacina <epbonacina@gmail.com>
2024-05-20 08:26:42 -0300

    A (very) slow solution for the first part of day 5.

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
-use std::{fs, result};
+use std::fs;
 
-// const FILE_PATH: &str = "input.txt";
-const FILE_PATH: &str = "smaller_input.txt";
+const FILE_PATH: &str = "input.txt";
+// const FILE_PATH: &str = "smaller_input.txt";
 const SECTION_NAMES: [&str; 7] = [
         "seed-to-soil map",
         "soil-to-fertilizer map",
@@ -12,6 +12,7 @@ const SECTION_NAMES: [&str; 7] = [
         "temperature-to-humidity map",
         "humidity-to-location map",
     ];
+
 
 fn read_input() -> Vec<String> {
     fs::read_to_string(FILE_PATH)
@@ -49,7 +50,7 @@ fn get_section_mappings(section_name: &str, sections: &Vec<String>) -> HashMap<u
         let range_len: u32 = values[2].parse().unwrap();
 
         for i in 0..range_len {
-            resulting_map.insert(dest_range_start + i, src_range_start + i);
+            resulting_map.insert(src_range_start + i, dest_range_start + i);
         }
     }
 
@@ -65,7 +66,17 @@ fn get_section_by_name(section_name: &str, sections: &Vec<String>) -> String {
 }
 
 
-fn find_seed_location(seed_id: u32, mappings: Vec<
+fn find_seed_location(seed_id: u32, mappings: &HashMap<&str, HashMap<u32, u32>>) -> u32 {
+    let mut key = seed_id;
+    for section in SECTION_NAMES {
+        let mapping = mappings.get(section).unwrap();
+        match mapping.get(&key) {
+            Some(k) => key = *k,
+            None => {},
+        }
+    }
+    key
+}
 
 fn main() {
     let sections = read_input();
@@ -76,11 +87,15 @@ fn main() {
         let mappings = get_section_mappings(&section_name, &sections);
         mappings_of_each_section.insert(section_name, mappings);
     }
-    println!("{:?}", mappings_of_each_section.get("seed-to-soil map").unwrap());
 
+    let mut lowest_location = i32::MAX;
     for seed_id in seed_ids {
-        let location_number = find_seed_location(seed_id, mappings_of_each_section);
+        let location_number = find_seed_location(seed_id, &mappings_of_each_section);
+        if (location_number as i32) < lowest_location {
+            lowest_location = location_number as i32;
+        }
     }
+    println!("{}", lowest_location);
 }
 
 
